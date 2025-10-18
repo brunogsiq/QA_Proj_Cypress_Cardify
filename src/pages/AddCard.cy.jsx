2b0037245f2de1697e0386222ed2aaf7ae4bf98e
brunogsiq <brunogsiq@gmail.com>
2025-10-17 22:19:12 -0300

    Correção de skip test

--- src/pages/AddCard.cy.jsx
+++ src/pages/AddCard.cy.jsx
@@ -185,7 +185,7 @@ context(`${contexto} - Teste de Componentes.`, () =>
 			cy.AlertError_HaveText('Selecione um banco');
 		});
 
-		it.skip(`${teste}.${++complemento} - Campos obrigatório preenchidos - Número + Nome + Data de Expiração + CVV do cartão.`, () =>
+		it(`${teste}.${++complemento} - Campos obrigatório preenchidos - Número + Nome + Data de Expiração + CVV do cartão.`, () =>
 		{
 			cy.contains('button', 'Adicionar').click();
 
@@ -208,7 +208,7 @@ context(`${contexto} - Teste de Componentes.`, () =>
 			cy.AlertError_HaveText('Selecione um banco');
 		});
 
-		it(`${teste}.${++complemento} - Campos obrigatório preenchidos - Número + Nome + Data de Expiração + CVV + Banco do cartão.`, () =>
+		it.skip(`${teste}.${++complemento} - Campos obrigatório preenchidos - Número + Nome + Data de Expiração + CVV + Banco do cartão.`, () =>
 		{
 			cy.contains('button', 'Adicionar').click();
 
